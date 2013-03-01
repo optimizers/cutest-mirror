b0feecc71b2eefc580e1abc4ef8eeea357e70eab
nimg <nimg@ca3beb79-f7c6-489d-80df-314e45fd556d>
2013-03-01 16:35:00 +0000

    interface to pennon added new tool cofsg added bugs fixed documentation updated
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/cutest/cutest/trunk@102 ca3beb79-f7c6-489d-80df-314e45fd556d

---
+++ src/pennon/pennon_test.f
@@ -0,0 +1,36 @@
+      SUBROUTINE PENNLPF( n, m_lin, m, nnzg, nnzh,
+     &                    X_l, X_u, C_l, C_u, X, Y, 
+     &                    PENNON_evalof, PENNON_evalog, PENNON_evaloh,
+     &                    PENNON_evalcf, PENNON_evalcg, PENNON_evalch,
+     &                    IOPTIONS, DOPTIONS, IRESULTS, DRESULTS, 
+     &                    status )
+      INTEGER :: n, m_lin, m, max_nnzg, max_nnzh, status
+      INTEGER :: IOPTIONS( 17 ), IRESULTS( 4 )
+      DOUBLE PRECISION, DIMENSION( n ) :: X, X_l, X_u
+      DOUBLE PRECISION, DIMENSION( m ) :: Y, C_l, C_u
+      DOUBLE PRECISION :: DOPTIONS( 13 ), DRESULTS( 5 )
+      EXTERNAL :: PENNON_evalof, PENNON_evalog, PENNON_evaloh
+      EXTERNAL :: PENNON_evalcf, PENNON_evalcg, PENNON_evalch
+C  local variables
+      DOUBLE PRECISION :: c
+C  automatic arrays
+      INTEGER :: G_var( nnzg ), H_row( nnzh ), H_col( nnzh )
+      DOUBLE PRECISION :: G_val( nnzg ), H_val( nnzh )
+C  trial calls
+      CALL PENNON_evalof( X, DRESULTS( 1 ) )
+      CALL PENNON_evalog( X, nnzg, G_var, G_val )
+      CALL PENNON_evaloh( X, nnzh, H_row, H_col, H_val )
+      CALL PENNON_evalcf( 0, X, DRESULTS( 3 ) )
+      CALL PENNON_evalcg( 0, X, nnzg, G_var, G_val )
+      CALL PENNON_evalch( 0, X, nnzh, H_row, H_col, H_val )
+      IRESULTS( 1 ) = 0
+      IRESULTS( 2 ) = 0
+      IRESULTS( 3 ) = 0
+      IRESULTS( 4 ) = 0
+      DRESULTS( 2 ) = 1.0D0
+      DRESULTS( 4 ) = Y( 1 ) * DRESULTS( 3 )
+      DRESULTS( 3 ) = ABS( DRESULTS( 3 ) )
+      DRESULTS( 5 ) = 1.0D0
+      status = 0
+      RETURN
+      END
