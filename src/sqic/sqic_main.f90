80120c6433c40e8f6e3a794d54ed7e3c20d490ba
nimg <nimg@ca3beb79-f7c6-489d-80df-314e45fd556d>
2014-02-28 10:18:16 +0000

    updates for new version of SQIC
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/cutest/cutest/trunk@213 ca3beb79-f7c6-489d-80df-314e45fd556d

--- src/sqic/sqic_main.f90
+++ src/sqic/sqic_main.f90
@@ -1,43 +1,38 @@
-!     ( Last modified on 26 Feb 2013 at 13:30:00 )
+!     ( Last modified on 28 Feb 2014 at 08:50:00 )
 
   program sqic_main
 
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- 
+
 !     Driver for running SQIC on CUTEst problems.
- 
-!     Derived from SQIC program sqicma.f90 written by 
+
+!     Derived from SQIC program sqicma.f90 written by
 !     Philip Gill and Elizabeth Wong
 !     CUTEst evolution February 2013, Nick Gould
-      
+!     updates February 2014, Elizabeth Wong
+
 !+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-! use snModulePrecision, only : ip
   use CUTEst_interface_double
-
-  use ic21mxv,           only : crd2spr
-  use sqModuleProb,      only : qpProb
 
   implicit none
 
   integer, parameter :: ip = kind( 1 ), rp = kind( 1.0D+0 )
   integer(ip), parameter :: iCutest = 55, iOut = 6, io_buffer = 11
   integer(ip), parameter :: iPrint = 9, iSumm  = 6, iSpecs = 4
-  integer(ip)   :: INFO, n, m, nm, nnH, ncObj, neA, lenA, neH, lenH, nS, nInf
-  integer(ip)   :: iObj, nName, status, alloc_stat
+  integer(ip)   :: INFO, n, m, nm, nnH, ncObj, neA, lenA, neH, neH2, lenH, nS
+  integer(ip)   :: iObj, nName, status, alloc_stat, j, nInf
   real(rp)      :: Obj, ObjAdd, sInf
   real(rp)      :: CPU( 2 ), CALLS( 7 )
   character(8)  :: Names(1)
   character(10) :: Prob
   character(20) :: filename
 
-  integer(ip),   pointer :: hs(:), hEtype(:), indA(:), locA(:)
-  integer(ip),   pointer :: indH(:), locH(:), irow(:), jcol(:)
+  integer(ip),   pointer :: hs(:), hEtype(:), indA(:), locA(:), jcol2(:)
+  integer(ip),   pointer :: indH(:), locH(:), irow(:), jcol(:), irow2(:)
   real(rp),      pointer :: bl(:), bu(:), x(:), pi(:), rc(:), cObj(:)
-  real(rp),      pointer :: valA(:), valH(:), zero(:), cval(:), b(:)
+  real(rp),      pointer :: valA(:), valH(:), zero(:), cval(:), cval2(:), b(:)
   logical,       pointer :: equation(:), linear(:)
   character(10), pointer :: vname(:), gname(:)
-  type(qpProb)           :: QP
 
   open ( iCutest, file = 'OUTSDIF.d', form = 'FORMATTED', status = 'old' )
   rewind( iCutest )
@@ -64,7 +59,7 @@
 
      call CUTEST_csetup( status, iCutest, iOut, io_buffer,                     &
                          n, m, x(1:n), bl(1:n), bu(1:n),                       &
-                         pi, bl(n+1:nm), bu(n+1:nm), equation, linear, 0, 0, 1 )
+                         pi, bl(n+1:nm), bu(n+1:nm), equation, linear, 0, 2, 1 )
      if ( status /= 0 ) go to 910
      deallocate ( equation, linear )
 
@@ -108,17 +103,37 @@
      if ( lenH > 0 ) then
         allocate ( cval(lenH), irow(lenH), jcol(lenH), stat = alloc_stat )
         if ( alloc_stat /= 0 ) GO TO 990
+
         call CUTEST_cish ( status, n, zero, 0, neH, lenH, cval, irow, jcol )
         if ( status /= 0 ) go to 910
+
+        allocate ( cval2(2*lenH), irow2(2*lenH), jcol2(2*lenH) )
+
+        neH2 = 0
+        do j = 1, neH
+           neH2 = neH2 + 1
+           cval2(neH2) = cval(j)
+           irow2(neH2) = irow(j)
+           jcol2(neH2) = jcol(j)
+
+           if ( irow(j) /= jcol(j) ) then
+              neH2 = neH2 + 1
+              cval2(neH2) = cval(j)
+              irow2(neH2) = jcol(j)
+              jcol2(neH2) = irow(j)
+           end if
+        end do
 
         deallocate ( zero )
 
         nnH = maxval ( jcol(1:neH) )
 
-        allocate ( indH(neH), locH(n+1), valH(neH), stat = alloc_stat )
+        allocate ( indH(neH2), locH(nnH+1), valH(neH2), stat = alloc_stat )
         if ( alloc_stat /= 0 ) GO TO 990
-        call crd2spr ( n, neH, cval, irow, jcol, valH, indH, locH )
+
+        call crd2spr ( nnH, neH2, cval2, irow2, jcol2, valH, indH, locH )
         deallocate ( irow, jcol, cval )
+        deallocate ( irow2, jcol2, cval2 )
      else
         nnH = 0
      end if
@@ -146,13 +161,16 @@
      deallocate ( vname )
 
      ! dummy row in A
-     neA = 1
+     neA = n
      allocate ( indA(neA), locA(n+1), valA(neA), stat = alloc_stat )
      if ( alloc_stat /= 0 ) GO TO 990
-     indA(1)   = 1
-     valA(1)   = 1.0_rp
-     locA      = 1
-     locA(n+1) = 2
+
+     do j = 1, n
+        indA(j) = 1
+        valA(j) = 1.0_rp
+        locA(j) = j
+     end do
+     locA(n+1) = neA+1
 
      x(n+1)    =  0.0
      bl(n+1)   = -1.0d+20
@@ -173,21 +191,39 @@
         call CUTEST_ush( status, n, zero, neH, lenH, cval, irow, jcol )
         if ( status /= 0 ) go to 910
 
+        allocate ( cval2(2*lenH), irow2(2*lenH), jcol2(2*lenH) )
+
+        neH2 = 0
+        do j = 1, neH
+           neH2 = neH2 + 1
+           cval2(neH2) = cval(j)
+           irow2(neH2) = irow(j)
+           jcol2(neH2) = jcol(j)
+
+           if ( irow(j) /= jcol(j) ) then
+              neH2 = neH2 + 1
+              cval2(neH2) = cval(j)
+              irow2(neH2) = jcol(j)
+              jcol2(neH2) = irow(j)
+           end if
+        end do
+
         deallocate ( zero )
 
         nnH = maxval ( jcol(1:neH) )
 
-        allocate ( indH(neH), locH(n+1), valH(neH), stat = alloc_stat )
+        allocate ( indH(neH2), locH(nnH+1), valH(neH2), stat = alloc_stat )
         if ( alloc_stat /= 0 ) GO TO 990
-        call crd2spr ( n, neH, cval, irow, jcol, valH, indH, locH )
+
+        call crd2spr ( n, neH2, cval2, irow2, jcol2, valH, indH, locH )
         deallocate ( irow, jcol, cval )
+        deallocate ( irow2, jcol2, cval2 )
 
      else
         nnH = 0
      end if
   end if
 
-! write(6,*) ' m, n ', m, n, Prob
 
   !-----------------------------------------------------------------------------
   ! Ok, we're done with CUTEst stuff.
@@ -202,17 +238,13 @@
   nName = 1
   hs    = 0
 
-  call qp%begin ( iPrint, iSumm )
-  call qp%specs ( iSpecs, INFO )
-
-  call qp%load ( Prob, m, n, nnH, iObj, ObjAdd,                                &
+  call ctSqic ( 'Cold', INFO, iPrint, iSumm, iSpecs, Prob,                     &
+                 m, n, nnH, iObj, ObjAdd,                                      &
                  neA, indA, locA, valA, bl, bu, ncObj, cObj,                   &
-                 nName, Names, hEtype, hs, x, pi, rc, neH, indH, locH, valH )
+                 nName, Names, hEtype, hs, x, pi, rc,                          &
+                 neH, indH, locH, valH,                                        &
+                 nS, nInf, sInf, Obj )
 
-  call qp%solve ( 'Cold', INFO, nS, nInf, sInf, Obj )
-!  call qp%solve ( 'Warm', INFO, nS, nInf, sInf, Obj )
-!  call qp%solve ( 'Hot', INFO, nS, nInf, sInf, Obj )
-  call qp%end
 
   call CUTEST_creport( status, CALLS, CPU )
   WRITE ( iOut, 2000 ) Prob, n, m, CALLS( 1 ), CALLS( 2 ),                     &
